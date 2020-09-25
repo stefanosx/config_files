ec9cd52d89bfd2038dab5d4c0b6f4023ffde96af
Stefanos Xanthopoulos <stefanosx@gmail.com>
2020-09-25 14:23:48 +0300

    Move .vim to another repo

--- install_script.sh
+++ install_script.sh
@@ -3,8 +3,7 @@
 # Bootstrap script for setting up a new OSX machine
 #
 # This should be idempotent so it can be run multiple times.
-
-
+#
 echo "Starting bootstrapping"
 
 # Check for Homebrew, install if we don't have it
@@ -13,18 +12,55 @@ if test ! $(which brew); then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 echo 'update brew'
-
 brew update
 
 # Install rbenv and ruby-build
+echo 'install rbenv and ruby-build'
 brew install rbenv ruby-build
 echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
+echo 'install vim'
+brew install vim --with-override-system-vi
+
+echo 'install tree'
 brew install tree
+
+echo 'install tmux'
 brew install tmux
 
-# Install pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo 'install gitx'
+brew cask install rowanj-gitx
+
+echo 'install docker'
+brew cask install docker
+
+echo 'install slack'
+brew cask install slack
+
+echo 'install chrome'
+brew cask install google-chrome
+
+echo 'install shiftit'
+brew cask install shiftit
+
+echo 'install copyclip'
+brew cask install copyclip
+
+echo 'install 1password'
+brew cask install 1password
+
+echo 'install spotify'
+brew cask install spotify
+
+echo 'install toggl track'
+brew cask install toggl-track
+
+echo 'install pathogen and vim plugins'
+git clone git@github.com:stefanosx/config_files.git ~/.vim &&
+  cd ~/.vim &&
+  git submodule update --init &&
+  echo 'source ~/.vim/.vimrc' > ~/.vimrc
 
 echo "Bootstrapping complete"
+
+
